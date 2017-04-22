3fee45008f17e027e140f63dd8dcd26adb2057c5
Taeyelor <Taeyelor@gmail.com>
2017-04-22 22:35:58 +0430

    Add project files.

---
+++ Sadjad-AI-CppClient-2017/Sadjad-AI-CppClient-2017.cpp
@@ -0,0 +1,11 @@
+// Sadjad-AI-CppClient-2017.cpp : main project file.
+
+#include "stdafx.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Console::WriteLine(L"Hello World");
+    return 0;
+}
